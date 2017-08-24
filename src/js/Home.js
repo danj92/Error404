8fd0b5c319bc084bbb37ea1d59ceaa2081aa5b93
Andriy Danylko <andriy.danylko@gmail.com>
2017-08-24 13:59:39 +0200

    all staff

---
+++ src/js/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>Home page Home.js</h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/js/Popular.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Popular = ({popular}) => {
+    return(
+        <div>Populat title in Popular.js</div>
+    );
+};
+
+export default Popular;
